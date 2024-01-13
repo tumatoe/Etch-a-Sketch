9d66c16f61134f3e7aa541917ff02b373a4911c4
Tumus <111464530+tumatoe@users.noreply.github.com>
2024-01-12 17:24:29 -0800

    base assignment done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let gridBox;
 let gridRow;
 const gridContainer = document.querySelector('.grid-Container');
 const buttonContainer = document.querySelector('.button-Container');
+let input;
+let gridSize;
 
 function createGrid() {
 
@@ -21,22 +23,51 @@ function createGrid() {
 }
 createGrid()
 
-gridContainer.addEventListener('mouseover', function(event) {
-    if (event.target.classList.contains('gridCell')) {
-        event.target.style.backgroundColor = 'black'
-    }
-})
-
 function addGridBtn() {
     const newGridBtn = document.createElement('button');
     newGridBtn.textContent = 'New Grid';
     buttonContainer.appendChild(newGridBtn);
     
     newGridBtn.addEventListener('click', function() {
-        prompt('What size grid would you like?')
+        let input = (prompt('What size grid would you like?'))
+        let gridSize = getUserGridSize(input);
+
+        if (gridSize !== null) {
+            gridContainer.innerHTML = '';
+            createNewGrid(gridSize);
+        }
     })
 }
 addGridBtn()
 
+function getUserGridSize(input) {
+    let gridSize = parseInt(input, 10);
+    
 
-let dsasdasdasasdasdasdasdas;
+    if ((!isNaN(gridSize)) && (gridSize <=100) && (gridSize >= 1)) {
+        return gridSize;
+    } else {
+        alert('Please enter a number from 1-100')
+        return null;
+    }
+}
+
+gridContainer.addEventListener('mouseover', function(event) {
+    if (event.target.classList.contains('gridCell')) {
+        event.target.style.backgroundColor = 'black'
+    }
+})
+
+function createNewGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.className = 'gridRow';
+
+        for ( let j = 0; j < gridSize; j++) {
+            let gridCell = document.createElement('div');
+            gridCell.className = 'gridCell'
+            gridRow.appendChild(gridCell)
+        }
+        gridContainer.appendChild(gridRow)
+    }
+}
